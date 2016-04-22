0c5fe2910e5b68ca3489adf60293f28b61df46d5
spaquet <stephan.paquet@duproprio.com>
2016-04-22 10:46:06 -0400

    Add lesson 6 & 7

---
+++ app/models/author.js
@@ -0,0 +1,20 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
+import Faker from 'faker';
+
+export default Model.extend({
+
+  name: attr('string'),
+
+  books: hasMany('book', {inverse: 'author', async: true}),
+  
+    isNotValid: Ember.computed.empty('name'),
+
+
+  randomize() {
+    this.set('name', Faker.name.findName());
+    return this;
+  }
+
+});
